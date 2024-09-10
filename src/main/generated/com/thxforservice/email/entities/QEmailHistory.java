31628dde8a5ea8558eaa7be0950a5a82ddd37321
beom33 <byby5546@naver.com>
2024-09-10 20:11:02 +0900

    수정사항 반영

--- src/main/generated/com/thxforservice/email/entities/QEmailHistory.java
+++ src/main/generated/com/thxforservice/email/entities/QEmailHistory.java
@@ -19,9 +19,21 @@ public class QEmailHistory extends EntityPathBase<EmailHistory> {
 
     public static final QEmailHistory emailHistory = new QEmailHistory("emailHistory");
 
+    public final StringPath _from = createString("_from");
+
     public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
 
+    public final DateTimePath<java.time.LocalDateTime> fileCreatedAt = createDateTime("fileCreatedAt", java.time.LocalDateTime.class);
+
+    public final StringPath fileNm = createString("fileNm");
+
+    public final NumberPath<Long> filesize = createNumber("filesize", Long.class);
+
     public final StringPath message = createString("message");
+
+    public final StringPath pathNm = createString("pathNm");
+
+    public final StringPath savedNm = createString("savedNm");
 
     public final NumberPath<Long> seq = createNumber("seq", Long.class);
 
--- src/main/java/com/thxforservice/email/entities/EmailHistory.java
+++ src/main/java/com/thxforservice/email/entities/EmailHistory.java
@@ -1,8 +1,10 @@
 package com.thxforservice.email.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -23,12 +25,35 @@ public class EmailHistory {
     @Column(name = "_to", length = 100)
     private String to;
 
+    @Column(name = "_from", length = 100)
+    private  String _from;
+
     private String subject;
 
     @Lob
     private String message;
 
+
+    @Column(name = "file_nm", length = 200)
+    private String fileNm;  // 원본 파일 이름
+
+    @Column(name = "saved_nm", length = 200)
+    private String savedNm;  // 저장된 파일 이름
+
+    @Column(name = "path_nm", length = 1000)
+    private String pathNm;  // 파일 경로
+
+    @Column(name = "filesize")
+    private Long filesize;  // 파일 크기 (바이트)
+
+    @CreatedDate
+    @Column(name = "file_created_at", updatable = false)
+    private LocalDateTime fileCreatedAt;  // 파일 생성일자
+
+
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+
 }
